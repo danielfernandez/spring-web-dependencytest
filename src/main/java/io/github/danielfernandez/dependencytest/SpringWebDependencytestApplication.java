b3bb2733ff7d9370a56974162639cd89501f8d40
Daniel Fernández <daniel@danfenz.dev>
2025-03-08 23:16:42 +0100

    Set application up for war deployment

--- src/main/java/io/github/danielfernandez/dependencytest/SpringWebDependencytestApplication.java
+++ src/main/java/io/github/danielfernandez/dependencytest/SpringWebDependencytestApplication.java
@@ -2,9 +2,16 @@ package io.github.danielfernandez.dependencytest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringWebDependencytestApplication {
+public class SpringWebDependencytestApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringWebDependencytestApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebDependencytestApplication.class, args);
